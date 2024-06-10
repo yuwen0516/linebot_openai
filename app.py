377415e7bae91dc7c8acccd149245a612783e49f
yuwen0516 <pa7361080516@gmail.com>
2024-06-10 21:57:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 
 # 替換為你的實際channel access token和channel secret
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('mJEbuHyFP8/Z+qMyD6Zhtxd8eY1403ryTIrNuoHSKz42f0TBlW3765kaLz/nRzrRA4sg4qEBH5oPQPa2TCz+CPR9w9tmoEpodOA0b7pHtas4auFyILcKO/fXE7upkK3RbwRu9mcDLYNQ55VMW002JgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('27f0cee2620555e6fc4e95338c69ad4e')
 
 @app.route("/callback", methods=['POST'])
 def callback():
